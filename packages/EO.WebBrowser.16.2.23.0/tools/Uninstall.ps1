a091c68cfb1852443c3a6f073b25a0753be0e2ab
Konrad K Sobon <ksobon1986@gmail.com>
2016-12-02 15:25:27 -0500

    package updates

--- packages/EO.WebBrowser.16.2.23.0/tools/Uninstall.ps1
+++
@@ -1,3 +0,0 @@
-param ($installPath, $toolsPath, $package, $project)
-
-Uninstall-EONuGetPackage $installPath $toolsPath $package $dte $project
---
+++ packages/EO.WebBrowser.16.2.72.0/tools/Install.ps1
@@ -0,0 +1,3 @@
+param ($installPath, $toolsPath, $package, $project)
+
+Install-EONuGetPackage $installPath $toolsPath $package $dte $project
